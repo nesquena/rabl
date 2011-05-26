e806bd2364749e8443f7fd90595be5d1f7473564
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-05-26 01:12:16 -0700

    add json_root false tests

--- test/engine_test.rb
+++ test/engine_test.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../teststrap', __FILE__)
+require File.expand_path('../../lib/rabl', __FILE__)
 require File.expand_path('../../lib/rabl/template', __FILE__)
 require File.expand_path('../models/user', __FILE__)
 
@@ -15,140 +16,297 @@ context "Rabl::Engine" do
     asserts_topic.assigns :_options
   end
 
-  context "#object" do
 
-    asserts "that it sets data source" do
-      template = rabl %q{
-        object @user
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new
-      template.render(scope)
-    end.matches "{\"user\":{}}"
+  context "with defaults" do
+    setup do
+      Rabl.configure do |config|
+        config.include_json_root     = true
+        config.include_xml_root      = false
+        config.enable_json_callbacks = false
+      end
+    end
 
-    asserts "that it can set root node" do
-      template = rabl %q{
-        object @user => :person
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new
-      template.render(scope)
-    end.equals "{\"person\":{}}"
+    context "#object" do
+
+      asserts "that it sets data source" do
+        template = rabl %q{
+          object @user
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new
+        template.render(scope)
+      end.matches "{\"user\":{}}"
+
+      asserts "that it can set root node" do
+        template = rabl %q{
+          object @user => :person
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new
+        template.render(scope)
+      end.equals "{\"person\":{}}"
+    end
+
+    context "#collection" do
+
+      asserts "that it sets object to be casted as a simple array" do
+        template = rabl %{
+          collection @users
+        }
+        scope = Object.new
+        scope.instance_variable_set :@users, [User.new, User.new]
+        template.render(scope)
+      end.equals "[{\"user\":{}},{\"user\":{}}]"
+
+      asserts "that it sets root node for objects" do
+        template = rabl %{
+          collection @users => :person
+        }
+        scope = Object.new
+        scope.instance_variable_set :@users, [User.new, User.new]
+        template.render(scope)
+      end.equals "{\"person\":[{\"person\":{}},{\"person\":{}}]}"
+
+    end
+
+    context "#attribute" do
+
+      asserts "that it adds an attribute or method to be included in output" do
+        template = rabl %{
+          object @user
+          attribute :name
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'irvine')
+        template.render(scope)
+      end.equals "{\"user\":{\"name\":\"irvine\"}}"
+
+      asserts "that it can add attribute under a different key name through :as" do
+        template = rabl %{
+          object @user
+          attribute :name, :as => 'city'
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'irvine')
+        template.render(scope)
+      end.equals "{\"user\":{\"city\":\"irvine\"}}"
+
+      asserts "that it can add attribute under a different key name through hash" do
+        template = rabl %{
+          object @user
+          attribute :name => :city
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'irvine')
+        template.render(scope)
+      end.equals "{\"user\":{\"city\":\"irvine\"}}"
+
+    end
+
+    context "#code" do
+
+      asserts "that it can create an arbitraty code node" do
+        template = rabl %{
+          code(:foo) { 'bar' }
+        }
+        template.render(Object.new)
+      end.equals "{\"foo\":\"bar\"}"
+
+      asserts "that it can be passed conditionals" do
+        template = rabl %{
+          code(:foo, :if => lambda { |i| false }) { 'bar' }
+        }
+        template.render(Object.new)
+      end.equals "{}"
+
+    end
+
+    context "#child" do
+
+      asserts "that it can create a child node" do
+        template = rabl %{
+          object @user
+          attribute :name
+          child(@user) { attribute :city }
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA')
+        template.render(scope)
+      end.equals "{\"user\":{\"name\":\"leo\",\"user\":{\"city\":\"LA\"}}}"
+
+      asserts "that it can create a child node with different key" do
+        template = rabl %{
+          object @user
+          attribute :name
+          child(@user => :person) { attribute :city }
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA')
+        template.render(scope)
+
+      end.equals "{\"user\":{\"name\":\"leo\",\"person\":{\"city\":\"LA\"}}}"
+    end
+
+    context "#glue" do
+
+      asserts "that it glues data from a child node" do
+        template = rabl %{
+          object @user
+          attribute :name
+          glue(@user) { attribute :city }
+          glue(@user) { attribute :age  }
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA', :age => 12)
+        template.render(scope)
+      end.equals "{\"user\":{\"name\":\"leo\",\"city\":\"LA\",\"age\":12}}"
+    end
   end
 
-  context "#collection" do
 
-    asserts "that it sets object to be casted as a simple array" do
-      template = rabl %{
-        collection @users
-      }
-      scope = Object.new
-      scope.instance_variable_set :@users, [User.new, User.new]
-      template.render(scope)
-    end.equals "[{\"user\":{}},{\"user\":{}}]"
+  context "without json root" do
+    setup do
+      Rabl.configure do |config|
+        config.include_json_root     = false
+        config.include_xml_root      = false
+        config.enable_json_callbacks = false
+      end
+    end
 
-    asserts "that it sets root node for objects" do
-      template = rabl %{
-        collection @users => :person
-      }
-      scope = Object.new
-      scope.instance_variable_set :@users, [User.new, User.new]
-      template.render(scope)
-    end.equals "{\"person\":[{\"person\":{}},{\"person\":{}}]}"
+    context "#object" do
 
+      asserts "that it sets data source" do
+        template = rabl %q{
+          object @user
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new
+        template.render(scope)
+      end.matches "{}"
+
+      asserts "that it can set root node" do
+        template = rabl %q{
+          object @user => :person
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new
+        template.render(scope)
+      end.equals "{}"
+    end
+
+    context "#collection" do
+
+      asserts "that it sets object to be casted as a simple array" do
+        template = rabl %{
+          collection @users
+        }
+        scope = Object.new
+        scope.instance_variable_set :@users, [User.new, User.new]
+        template.render(scope)
+      end.equals "[{},{}]"
+
+      asserts "that it sets root node for objects" do
+        template = rabl %{
+          collection @users => :person
+        }
+        scope = Object.new
+        scope.instance_variable_set :@users, [User.new, User.new]
+        template.render(scope)
+      end.equals "{\"person\":[{},{}]}"
+
+    end
+
+    context "#attribute" do
+
+      asserts "that it adds an attribute or method to be included in output" do
+        template = rabl %{
+          object @user
+          attribute :name
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'irvine')
+        template.render(scope)
+      end.equals "{\"name\":\"irvine\"}"
+
+      asserts "that it can add attribute under a different key name through :as" do
+        template = rabl %{
+          object @user
+          attribute :name, :as => 'city'
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'irvine')
+        template.render(scope)
+      end.equals "{\"city\":\"irvine\"}"
+
+      asserts "that it can add attribute under a different key name through hash" do
+        template = rabl %{
+          object @user
+          attribute :name => :city
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'irvine')
+        template.render(scope)
+      end.equals "{\"city\":\"irvine\"}"
+
+    end
+
+    context "#code" do
+
+      asserts "that it can create an arbitraty code node" do
+        template = rabl %{
+          code(:foo) { 'bar' }
+        }
+        template.render(Object.new)
+      end.equals "{\"foo\":\"bar\"}"
+
+      asserts "that it can be passed conditionals" do
+        template = rabl %{
+          code(:foo, :if => lambda { |i| false }) { 'bar' }
+        }
+        template.render(Object.new)
+      end.equals "{}"
+
+    end
+
+    context "#child" do
+
+      asserts "that it can create a child node" do
+        template = rabl %{
+          object @user
+          attribute :name
+          child(@user) { attribute :city }
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA')
+        template.render(scope)
+      end.equals "{\"name\":\"leo\",\"user\":{\"city\":\"LA\"}}"
+
+      asserts "that it can create a child node with different key" do
+        template = rabl %{
+          object @user
+          attribute :name
+          child(@user => :person) { attribute :city }
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA')
+        template.render(scope)
+
+      end.equals "{\"name\":\"leo\",\"person\":{\"city\":\"LA\"}}"
+    end
+
+    context "#glue" do
+
+      asserts "that it glues data from a child node" do
+        template = rabl %{
+          object @user
+          attribute :name
+          glue(@user) { attribute :city }
+          glue(@user) { attribute :age  }
+        }
+        scope = Object.new
+        scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA', :age => 12)
+        template.render(scope)
+      end.equals "{\"name\":\"leo\",\"city\":\"LA\",\"age\":12}"
+    end
   end
-
-  context "#attribute" do
-
-    asserts "that it adds an attribute or method to be included in output" do
-      template = rabl %{
-        object @user
-        attribute :name
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new(:name => 'irvine')
-      template.render(scope)
-    end.equals "{\"user\":{\"name\":\"irvine\"}}"
-
-    asserts "that it can add attribute under a different key name through :as" do
-      template = rabl %{
-        object @user
-        attribute :name, :as => 'city'
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new(:name => 'irvine')
-      template.render(scope)
-    end.equals "{\"user\":{\"city\":\"irvine\"}}"
-
-    asserts "that it can add attribute under a different key name through hash" do
-      template = rabl %{
-        object @user
-        attribute :name => :city
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new(:name => 'irvine')
-      template.render(scope)
-    end.equals "{\"user\":{\"city\":\"irvine\"}}"
-
-  end
-
-  context "#code" do
-
-    asserts "that it can create an arbitraty code node" do
-      template = rabl %{
-        code(:foo) { 'bar' }
-      }
-      template.render(Object.new)
-    end.equals "{\"foo\":\"bar\"}"
-
-    asserts "that it can be passed conditionals" do
-      template = rabl %{
-        code(:foo, :if => lambda { |i| false }) { 'bar' }
-      }
-      template.render(Object.new)
-    end.equals "{}"
-
-  end
-
-  context "#child" do
-
-    asserts "that it can create a child node" do
-      template = rabl %{
-        object @user
-        attribute :name
-        child(@user) { attribute :city }
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA')
-      template.render(scope)
-    end.equals "{\"user\":{\"name\":\"leo\",\"user\":{\"city\":\"LA\"}}}"
-
-    asserts "that it can create a child node with different key" do
-      template = rabl %{
-        object @user
-        attribute :name
-        child(@user => :person) { attribute :city }
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA')
-      template.render(scope)
-
-    end.equals "{\"user\":{\"name\":\"leo\",\"person\":{\"city\":\"LA\"}}}"
-  end
-
-  context "#glue" do
-
-    asserts "that it glues data from a child node" do
-      template = rabl %{
-        object @user
-        attribute :name
-        glue(@user) { attribute :city }
-        glue(@user) { attribute :age  }
-      }
-      scope = Object.new
-      scope.instance_variable_set :@user, User.new(:name => 'leo', :city => 'LA', :age => 12)
-      template.render(scope)
-    end.equals "{\"user\":{\"name\":\"leo\",\"city\":\"LA\",\"age\":12}}"
-  end
-
 end
