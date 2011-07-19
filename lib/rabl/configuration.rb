ae108f573b70480dd79f47ea0dfd4f1b7c84eaad
Benjamin Yu <benjaminlyu@gmail.com>
2011-07-19 15:34:00 -0700

    Adds optional msgpack serialization format to Rabl.
    Details
    * Fixes #68 (nesquena/rabl issues tracker)
    * Message Pack: http://www.msgpack.org/
    * Defaults to using the Ruby msgpack gem: http://rubygems.org/gems/msgpack
    * Adds Rabl configuration option `include_msgpack_root` which has same semantics as include_json_root.
    * Adds Rabl configuration option `msgpack_engine` which has similar semantics as json_engine.
    * Adds whole test suite that is essentially a clone of engine_test.rb, but with proper msgpack expected values.
      Note that I converted the JSON expected values to their msgpack equivalent in this process.
    * Updated README.md to reflect the msgpack changes.

--- lib/rabl/configuration.rb
+++ lib/rabl/configuration.rb
@@ -1,25 +1,41 @@
+# We load the msgpack library if it is available.
+begin
+  require 'msgpack'
+rescue LoadError
+end
+
 module Rabl
   # Rabl.host
   class Configuration
     attr_accessor :include_json_root
+    attr_accessor :include_msgpack_root
     attr_accessor :include_xml_root
     attr_accessor :enable_json_callbacks
     attr_writer   :json_engine
+    attr_writer   :msgpack_engine
     attr_writer   :xml_options
 
     DEFAULT_XML_OPTIONS = { :dasherize  => true, :skip_types => false }
 
     def initialize
       @include_json_root     = true
+      @include_msgpack_root  = true
       @include_xml_root      = false
       @enable_json_callbacks = false
       @json_engine           = nil
+      @msgpack_engine        = nil
       @xml_options           = {}
     end
 
     # Returns the multi_json engine for use with RABL
     def json_engine
       @json_engine || MultiJson.engine
+    end
+
+    ##
+    # @return the MessagePack encoder/engine to use.
+    def msgpack_engine
+      @msgpack_engine || ::MessagePack
     end
 
     # Allows config options to be read like a hash
