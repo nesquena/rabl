62d9d3eeb5f99d516a4a43834e26466e5ea2a917
Kyle Fleming <kyle@rexly.com>
2011-09-22 23:35:19 -0700

    fix failing unit test from merge commit d4b6e78c9d, pull request #103

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -30,37 +30,39 @@ context "Rabl::Helpers" do
   end
 
   context "fetch_source" do
+    helper(:tmp_path) { @tmp_path ||= Pathname.new(Dir.mktmpdir) }
+
     setup do
       Rails = stub(Class.new)
-      Dir.mktmpdir do |dir|
-        tmp_path = Pathname.new(dir)
-        Rails.root.returns(tmp_path)
-        File.open(tmp_path + "test.rabl", "w") do |f|
-          f.puts "content"
-        end
-        [TestHelper.new.fetch_source('test', :view_path => tmp_path.to_s)]
+      Rails.root.returns(tmp_path)
+      File.open(tmp_path + "test.rabl", "w") do |f|
+        f.puts "content"
       end
+      TestHelper.new.fetch_source('test', :view_path => tmp_path.to_s)
     end
-    asserts('detects file.rabl') { topic.first }.equals "content\n"
+    asserts('detects file.rabl') { topic }.equals do
+      ["content\n", (tmp_path + 'test.rabl').to_s]
+    end
     teardown { Object.send(:remove_const, :Rails) }
   end
 
   context "fetch_source" do
+    helper(:tmp_path) { @tmp_path ||= Pathname.new(Dir.mktmpdir) }
+
     setup do
       Rails = stub(Class.new)
-      Dir.mktmpdir do |dir|
-        tmp_path = Pathname.new(dir)
-        Rails.root.returns(tmp_path)
-        File.open(tmp_path + "test.rabl", "w") do |f|
-          f.puts "content"
-        end
-        File.open(tmp_path + "test.json.rabl", "w") do |f|
-          f.puts "content2"
-        end
-        [TestHelper.new.fetch_source('test', :view_path => tmp_path.to_s)]
+      Rails.root.returns(tmp_path)
+      File.open(tmp_path + "test.rabl", "w") do |f|
+        f.puts "content"
       end
+      File.open(tmp_path + "test.json.rabl", "w") do |f|
+        f.puts "content2"
+      end
+      TestHelper.new.fetch_source('test', :view_path => tmp_path.to_s)
     end
-    asserts('detects file.json.rabl first') { topic.first }.equals "content2\n"
+    asserts('detects file.json.rabl first') { topic }.equals do
+      ["content2\n", (tmp_path + 'test.json.rabl').to_s]
+    end
     teardown { Object.send(:remove_const, :Rails) }
   end
 end
