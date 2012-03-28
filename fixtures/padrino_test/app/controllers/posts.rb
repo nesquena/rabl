7d3ee7f80c0bad28ce0cb28d978c359cd7c0c5e6
David Sommers <databyte@gmail.com>
2012-03-28 17:09:02 -0400

    add the ability to render html, BREAKS TESTS
    * shows problem with Rails 3.2 rendering templates
      BREAKS RAILS3 and RAILS3_2 tests
    
    * THIS BREAKS PADRINO TESTS (need help to fix)
    
    ~/.rvm/gems/ruby-1.9.3-p125@rabl/gems/actionpack-3.2.2/lib/action_view/path_set.rb:58:in `find': Missing template /index with {:locale=>[:en], :formats=>[:json], :handlers=>[:erb, :builder, :coffee, :rabl]}. Searched in: (ActionView::Template::Error)

--- fixtures/padrino_test/app/controllers/posts.rb
+++ fixtures/padrino_test/app/controllers/posts.rb
@@ -1,11 +1,11 @@
 PadrinoTest.controllers :posts do
   get :index do
     @posts = Post.order("id ASC")
-    render "posts/index"
+    render :rabl, "posts/index"
   end
 
   get :show, :map => "/posts", :with => :id do
     @post = Post.find(params[:id])
-    render "posts/show"
+    render :rabl, "posts/show"
   end
 end
