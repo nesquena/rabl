c337f1d35f90089e14c512614f16b1e6cadeb942
Nathan Esquenazi <nesquena@gmail.com>
2012-03-30 01:08:08 -0700

    Fixes padrino renders back to original style

--- fixtures/padrino_test/app/controllers/posts.rb
+++ fixtures/padrino_test/app/controllers/posts.rb
@@ -1,11 +1,11 @@
 PadrinoTest.controllers :posts do
   get :index do
     @posts = Post.order("id ASC")
-    render :rabl, "posts/index"
+    render "posts/index"
   end
 
   get :show, :map => "/posts", :with => :id do
     @post = Post.find(params[:id])
-    render :rabl, "posts/show"
+    render "posts/show"
   end
 end
