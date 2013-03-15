2ea907df66f70b67e4c1f38e8133e3764e44c5a4
Alexander Kaupanin <kaupanin@gmail.com>
2013-03-15 10:18:40 +0400

    Passing locals when rendering templates via partials or inheritance

--- fixtures/ashared/views_rails_3/users/phone_number.json.rabl
+++ fixtures/ashared/views_rails_3/users/phone_number.json.rabl
@@ -1,6 +1,8 @@
 attributes :prefix, :suffix, :area_code
 attributes :is_primary => :primary
 
-node :formatted do |n|
-  n.formatted
+if locals[:revert]
+  node(:reverted) { |n| n.formatted.reverse }
+else
+  node(:formatted) { |n| n.formatted }
 end
--- lib/rabl/engine.rb
+++ lib/rabl/engine.rb
@@ -20,6 +20,7 @@ module Rabl
     # Renders the representation based on source, object, scope and locals
     # Rabl::Engine.new("...source...", { :format => "xml" }).render(scope, { :foo => "bar", :object => @user })
     def render(scope, locals, &block)
+      locals.merge! locals.delete(:locals) || {}
       reset_options!
       @_locals, @_scope = locals, scope
       self.copy_instance_variables_from(@_scope, [:@assigns, :@helpers])
