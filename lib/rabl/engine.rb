75bc5b769293b9f3ad502b76321d068cf4088c27
Teng Siong Ong <siong1987@gmail.com>
2013-05-04 23:49:24 -0700

    Special `cache_key` is being used by rails 4 from the custom `Rabl::Digester`.

--- lib/rabl/engine.rb
+++ lib/rabl/engine.rb
@@ -274,11 +274,24 @@ module Rabl
       _cache = @_cache if defined?(@_cache)
       cache_key, cache_options = *_cache || nil
       if template_cache_configured? && cache_key
-        result_cache_key = Array(cache_key) + [@_options[:root_name], @_options[:format]]
+        if Rails.version =~ /^[4]/
+          result_cache_key = cache_key_with_digest(cache_key)
+        else
+          result_cache_key = Array(cache_key) + [@_options[:root_name], @_options[:format]]
+        end
+
         fetch_result_from_cache(result_cache_key, cache_options, &block)
       else # skip caching
         yield
       end
+    end
+
+    def cache_key_with_digest(cache_key)
+      Array(cache_key) + [
+        @_options[:root_name],
+        @_options[:format],
+        Digestor.digest(@virtual_path, :rabl, lookup_context, dependencies: view_cache_dependencies)
+      ]
     end
 
   end
