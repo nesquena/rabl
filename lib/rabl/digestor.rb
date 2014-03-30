d8d1824bf1b284ee4bb5a97aeaeabeaf64fe3c84
Washington Luiz <huoxito@gmail.com>
2014-03-30 11:08:00 -0300

    Update ActionView::Digestor#digest for rails 4.1.0
    format is not passed as an argument anymore and it only takes a hash
    instead of 4 arguments
    
    See https://github.com/rails/rails/commit/767a2b009c07c5c3e4b77a77942f11701f1d8858

--- lib/rabl/digestor.rb
+++ lib/rabl/digestor.rb
@@ -2,17 +2,30 @@ module Rabl
   class Digestor < ActionView::Digestor
     # Override the original digest function to ignore partial which
     # rabl doesn't use the Rails conventional _ symbol.
-    def self.digest(name, format, finder, options = {})
-      cache_key = [name, format] + Array.wrap(options[:dependencies])
-      @@cache[cache_key.join('.')] ||= begin
-        Digestor.new(name, format, finder, options).digest
+    if Rails.version.to_s >= '4.1'
+      def self.digest(options = {})
+        cache_key = [options[:name]] + Array.wrap(options[:dependencies])
+        @@cache[cache_key.join('.')] ||= begin
+          Digestor.new({ name: options[:name], finder: options[:finder] }.merge!(options)).digest
+        end
+      end
+    else
+      def self.digest(name, format, finder, options = {})
+        cache_key = [name, format] + Array.wrap(options[:dependencies])
+        @@cache[cache_key.join('.')] ||= begin
+          Digestor.new(name, format, finder, options).digest
+        end
       end
     end
 
     private
       def dependency_digest
         template_digests = dependencies.collect do |template_name|
-          Digestor.digest(template_name, format, finder)
+          if Rails.version.to_s >= '4.1'
+            Digestor.digest(name: template_name, finder: finder)
+          else
+            Digestor.digest(template_name, format, finder)
+          end
         end
 
         (template_digests + injected_dependencies).join("-")
